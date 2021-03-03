0d3b619c065c1d34c97f856a864a7f0be2947861
Travis Glover <glovertravis@gmail.com>
2021-03-02 18:09:40 -0800

    nginx - 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://testing.glovertravis.com/cable'
   # config.action_cable.allowed_request_origins = 'https://testing.glovertravis.com'
-  # config.action_cable.url = 'ws://testing.glovertravis.com/cable' # ws:// is non-secure, wss:// is secure
-  # config.action_cable.allowed_request_origins = [ 'http://testing.glovertravis.com' ]
+  config.action_cable.url = 'wss://solutionsjournalism.glovertravis.com/cable'
+  config.action_cable.allowed_request_origins = ["https://solutionsjournalism.glovertravis.com", "testing.glovertravis.com"]
   config.action_cable.disable_request_forgery_protection = true
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
